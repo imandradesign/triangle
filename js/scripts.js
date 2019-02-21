5a9861554733730bb2216f3f8c4eb047ea7b348e
Imandra McKenzie and Lex Wilkins <imandra@>
2019-02-20 20:43:15 -0800

    Add reload button to hide results and update styles.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   $('#equi').hide();
   $('#scl').hide();
   $('#not-triangle').hide();
+  $('#reload').hide();
 
   $('.triangle-form form').submit(function(){
     var num1 = parseInt($('input#num1').val());
@@ -12,21 +13,29 @@ $(document).ready(function(){
 
     if (num1 + num2 <= num3 || num1 + num3 <= num2 || num2 + num3 <= num1) {
       $("#not-triangle").show();
+      $('#reload').show();
+      $('.triangle-form form').hide();
       $('#iso').hide();
       $('#equi').hide();
       $('#scl').hide();
     } else if (num1 === num2 && num1 === num3) {
       $('#equi').show();
+      $('#reload').show();
+      $('.triangle-form form').hide();
       $('#iso').hide();
       $('#scl').hide();
       $('#not-triangle').hide();
     } else if (num1 === num2 || num1 === num3 || num2 === num3) {
       $("#iso").show();
+      $('#reload').show();
+      $('.triangle-form form').hide();
       $('#equi').hide();
       $('#scl').hide();
       $('#not-triangle').hide();
     } else if (num1 !== num2 && num1 !== num3 && num2 !== num3) {
       $("#scl").show();
+      $('#reload').show();
+      $('.triangle-form form').hide();
       $('#iso').hide();
       $('#equi').hide();
       $('#not-triangle').hide();
@@ -34,4 +43,13 @@ $(document).ready(function(){
 
     event.preventDefault();
   });
+
+  $('#reload').click(function(event){
+    $('#iso').hide();
+    $('#equi').hide();
+    $('#scl').hide();
+    $('#not-triangle').hide();
+    $('.triangle-form form').show();
+    $('#reload').hide();
+  })
 });
